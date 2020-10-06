97a6c88ee597fe8b10c6b5ac45ea17e4759adb7b
JJuDD17 <34658105+JJuDD17@users.noreply.github.com>
2020-10-06 23:02:06 +0300

    Update heroku_schedule_test.py

--- heroku_schedule_test.py
+++ heroku_schedule_test.py
@@ -3,6 +3,7 @@ from requests import get
 import os
 import datetime as dt
 import time
+import random
 
 print('heroku_schedule_test.py is running')
 
@@ -28,7 +29,22 @@ def sleep_job():
     time.sleep(60)
     now = dt.datetime.now().strftime('%d.%m.%Y %H:%M:%S')
     print(f'[{now}] sleeped 60 seconds')
-    
+
+def imitate_work_job(seconds):
+    def wrapper():
+        now = dt.datetime.now().strftime('%d.%m.%Y %H:%M:%S')
+        print(f'[{now}] imitating work for {seconds} seconds...')
+        start = time.time()
+        arr = []
+        while time.time() - start < seconds:
+            arr.append(random.randint(-2**31, 2**31-1))
+            if random.randint(1, 200000) == 1 or len(arr) == 2000000:
+                del arr
+                arr = []
+        now = dt.datetime.now().strftime('%d.%m.%Y %H:%M:%S')
+        print(f'[{now}] done imitating work')
+    return wrapper
+
 #@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
 #def scheduled_job():
 #    print('This job is run every weekday at 5pm.')
@@ -36,8 +52,8 @@ def sleep_job():
 #notify_job()
 #sched.add_job(notify_job, 'interval', minutes=3)
 #sched.add_job(notify_job, 'cron', hour=17, minute=53)
-sched.add_job(sleep_job, 'interval', minutes=2)
-sleep_job()
+sched.add_job(imitate_work_job(3*60), 'interval', minutes=4)
+imitate_work_job(3*60)()
 sched.start()
 #send_notification(TELEGRAM_API_TOKEN, TELEGRAM_CHAT_ID, TELEGRAM_MESSAGE)
 print('heroku_schedule_test.py is stopping')
